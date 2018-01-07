5064857b4ed3a09e10a5ec3b7dc8978fa81a8af7
Hiroaki Takisawa <takisawa@hit-start.jp>
2018-01-07 12:51:34 +0900

    Haskellのwarning対応

--- quicksort/quicksort.hs
+++ quicksort/quicksort.hs
@@ -1,7 +1,10 @@
+{-# OPTIONS -Wall -Werror #-}
 
-quicksort :: (Ord a) => [a] -> [a]
+quicksort :: [Integer] -> [Integer]
 quicksort [] = []
 quicksort [x] = [x]
 quicksort (pivot:xs) = quicksort [x | x <- xs, x <= pivot] ++ [pivot] ++ quicksort [x | x <- xs, x > pivot]
 
+
+main :: IO ()
 main = putStrLn $ show $ quicksort [4, 5, 2, 1, 3]
