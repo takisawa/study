cecf978363563ace382e135017ddfcc8717e43e0
Hiroaki Takisawa <takisawa@hit-start.jp>
2018-03-21 19:39:20 +0900

    目的関数、標準化関数を追加

--- ml-math/sample-5-2-1.py
+++ ml-math/sample-5-2-1.py
@@ -7,3 +7,23 @@ train_y = train[:,1]
 
 plt.plot(train_x, train_y, 'o')
 plt.show()
+
+theta0 = np.random.rand()
+theta1 = np.random.rand()
+
+def f(x):
+    return theta0 + theta1 * x
+
+def E(x, y):
+    return 0.5 * np.sum((y- f(x)) ** 2)
+
+mu = train_x.mean()
+sigma = train_x.std()
+
+def standardize(x):
+    return (x - mu) / sigma
+
+train_z = standardize(train_x)
+
+plt.plot(train_z, train_y, 'o')
+plt.show()
