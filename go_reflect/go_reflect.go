46aae59793451eb9400a5c86363581cf11034a24
Hiroaki Takisawa <takisawa@hit-start.jp>
2018-05-26 07:30:19 +0900

    Add reflect.Type samples.

--- go_reflect/go_reflect.go
+++ go_reflect/go_reflect.go
@@ -11,10 +11,20 @@ type Person struct {
 }
 
 func main() {
+	// reflect.Typeof
+	fmt.Println(reflect.TypeOf(3))
+	fmt.Println(reflect.TypeOf("ABC"))
+	fmt.Println(reflect.TypeOf([]int{1, 2, 3}))
+	fmt.Println(reflect.TypeOf(map[string]int{"A": 1, "B": 2, "C": 3}))
+	fmt.Println(reflect.TypeOf(Person{}))
+	fmt.Println(reflect.TypeOf(&Person{}))
+
+	// reflect.New
 	person := &Person{}
 	personType := reflect.TypeOf(person)
+	reflectedType := reflect.New(personType)
 
 	fmt.Printf("person: %#v\n", person)
 	fmt.Printf("personType: %#v\n", personType)
-	fmt.Printf("reflect.New(personType): %#v\n", reflect.New(personType))
+	fmt.Printf("reflectedType: %#v\n", reflectedType)
 }
