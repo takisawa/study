ed1073742ccec7d4172fd12453e1de23ba687d73
Hiroaki Takisawa <takisawa@hit-start.jp>
2018-06-17 10:19:42 +0900

    Add pandas dsintro codes.

---
+++ pandas/dsintro.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+
+# From ndarray
+print(np.random.randn(5))
+
+s = pd.Series(np.random.randn(5), index=['a','b','c','d','e'])
+
+print(s)
+print(s.index)
+
+print(pd.Series(np.random.randn(5)))
+
+
+
+# from dict
+d = {'b' : 1, 'a' : 0, 'c' : 2}
+print(pd.Series(d))
+
