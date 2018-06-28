60bc59e2341d894cb0aa9c8254a879fda9acf50d
Hiroaki Takisawa <takisawa@hit-start.jp>
2018-06-28 22:35:49 +0900

    Add python today sample.

--- python/timezone.py
+++ python/timezone.py
@@ -1,9 +1,11 @@
 
 from datetime import datetime
 from datetime import timezone
+from datetime import date
 
 # 環境変数 TZ で制御できる
 print(datetime.now())
+print(date.today())
 
 # 常にUTCの時刻を表示
 print(datetime.now(timezone.utc))
