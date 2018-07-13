9ef50a8e9f478c7e1a22030dd874b5cc00ead1c2
Hiroaki Takisawa <takisawa@hit-start.jp>
2018-07-13 22:35:00 +0900

    Add typehint List sample.

--- python/typehint.py
+++ python/typehint.py
@@ -1,6 +1,13 @@
+from typing import List
 
 def greeting(name: str) -> str:
     return 'Hello ' + name
 
 
 print(greeting('taro'))
+
+
+def greetings(names: List) -> str:
+    return ', '.join(['Hello ' + n for n in names])
+
+print(greetings(['taro', 'hanako']))
