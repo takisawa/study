4a198d67eeec8321e66933bdaedc290bed616e2d
Hiroaki Takisawa <takisawa@hit-start.jp>
2019-09-13 23:28:21 +0900

    Add init file for InfluxDB sample.

---
+++ influxdb/go.mod
@@ -0,0 +1,5 @@
+module github.com/takisawa/study/influxdb
+
+go 1.12
+
+require github.com/influxdata/influxdb1-client v0.0.0-20190809212627-fc22c7df067e // indirect
---
+++ influxdb/importer.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/influxdata/influxdb1-client" // this is important because of the bug in go mod
+	client "github.com/influxdata/influxdb1-client/v2"
+)
+
+func main() {
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr: "http://localhost:8086",
+	})
+	if err != nil {
+		fmt.Println("Error creating InfluxDB Client: ", err.Error())
+	}
+	defer c.Close()
+
+	q := client.NewQuery("SELECT count(value) FROM cpu_load", "mydb", "")
+	if response, err := c.Query(q); err == nil && response.Error() == nil {
+		fmt.Println(response.Results)
+	}
+}
